b2dd642d4a7fbc337681201ca40e2474bab3e9c3
itenpascal <90401249+itenpascal@users.noreply.github.com>
2022-11-27 16:28:40 +0100

    File changed to template with already helpful code for DMA etc

---
+++ Receiver/QAMDecGen/qamdec.c
@@ -0,0 +1,82 @@
+/*
+* qamdec.c
+*
+* Created: 05.05.2020 16:38:25
+*  Author: Chaos
+*/ 
+
+#include "avr_compiler.h"
+#include "pmic_driver.h"
+#include "TC_driver.h"
+#include "clksys_driver.h"
+#include "sleepConfig.h"
+#include "port_driver.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "semphr.h"
+#include "stack_macros.h"
+
+#include "mem_check.h"
+#include "errorHandler.h"
+
+#include "qaminit.h"
+#include "qamdec.h"
+
+QueueHandle_t decoderQueue;
+
+void vQuamDec(void* pvParameters)
+{
+	( void ) pvParameters;
+	
+	decoderQueue = xQueueCreate( 4, NR_OF_SAMPLES * sizeof(int16_t) );
+	
+	while(evDMAState == NULL) {
+		vTaskDelay(3/portTICK_RATE_MS);
+	}
+	
+	uint16_t bufferelement[NR_OF_SAMPLES];									// 32 Samples max
+	uint16_t peakelement/*[64]*/=0;												// 64 Samples
+	uint16_t peak = 0;
+	
+	xEventGroupWaitBits(evDMAState, DMADECREADY, false, true, portMAX_DELAY);
+	for(;;) {
+		while(uxQueueMessagesWaiting(decoderQueue) > 0) {
+			if(xQueueReceive(decoderQueue, &bufferelement[0], portMAX_DELAY) == pdTRUE) {
+				for(int a = 0; a < NR_OF_SAMPLES; a++) 	{
+					//peakelement[a] = bufferelement[a];
+					if (bufferelement[a] > peakelement) {
+						peakelement/*[a]*/ = bufferelement[a];
+						peak = a;
+						vDisplayClear();
+						vDisplayWriteStringAtPos(2,0,"%d",peak);
+						//vTaskDelay(500);
+					}
+				}
+				//vTaskDelay(10);
+				//Decode Buffer
+			}
+		}		
+		vTaskDelay( 2 / portTICK_RATE_MS );
+	}
+}
+
+void fillDecoderQueue(uint16_t buffer[NR_OF_SAMPLES])
+{
+	BaseType_t xTaskWokenByReceive = pdFALSE;
+	xQueueSendFromISR( decoderQueue, &buffer[0], &xTaskWokenByReceive );
+}
+
+ISR(DMA_CH2_vect)
+{
+	DMA.CH2.CTRLB|=0x10;
+	fillDecoderQueue( &adcBuffer0[0] );
+}
+
+ISR(DMA_CH3_vect)
+{
+	DMA.CH3.CTRLB |= 0x10;
+	fillDecoderQueue( &adcBuffer1[0] );
+}

22056a5151855f8a54750bfaa26ae5a5e3bca3c5
itenpascal <90401249+itenpascal@users.noreply.github.com>
2022-11-29 18:57:35 +0100

    small changes

--- Receiver/QAMDecGen/qamdec.c
+++ Receiver/QAMDecGen/qamdec.c
@@ -38,22 +38,41 @@ void vQuamDec(void* pvParameters)
 	}
 	
 	uint16_t bufferelement[NR_OF_SAMPLES];									// 32 Samples max
-	uint16_t peakelement/*[64]*/=0;												// 64 Samples
-	uint16_t peak = 0;
+	uint16_t negPeakelement/*[64]*/= 0;										// 64 Samples
+	uint16_t posPeakelement = 0;
+	uint16_t posPeak[18] = {NULL};
+	uint16_t negPeak = 0;
+	uint64_t runner = 0;													// runner, just counts up by Nr_of_samples to 2^64
+	int sigCount = 0;
 	
 	xEventGroupWaitBits(evDMAState, DMADECREADY, false, true, portMAX_DELAY);
 	for(;;) {
 		while(uxQueueMessagesWaiting(decoderQueue) > 0) {
 			if(xQueueReceive(decoderQueue, &bufferelement[0], portMAX_DELAY) == pdTRUE) {
 				for(int a = 0; a < NR_OF_SAMPLES; a++) 	{
-					//peakelement[a] = bufferelement[a];
-					if (bufferelement[a] > peakelement) {
-						peakelement/*[a]*/ = bufferelement[a];
-						peak = a;
-						vDisplayClear();
-						vDisplayWriteStringAtPos(2,0,"%d",peak);
-						//vTaskDelay(500);
+					runner++;
+					if (a == 0) {
+						negPeakelement = 10000;
+						posPeakelement = 0;
+						//posPeak = 0;
+						//negPeak = 0;
 					}
+					if (bufferelement[a] > posPeakelement) {
+						posPeakelement = bufferelement[a];
+						posPeak[sigCount] = runner;													// to save the peakround
+					}
+						if (bufferelement[a] <= 0.90 * posPeakelement ) {												
+							posPeakelement = 0;
+							sigCount++;																// to save the 18Bit needed for peak
+							if (sigCount >=18) {
+								sigCount = 0;
+							}
+						}
+					//}
+					if (bufferelement[a] < negPeakelement) {
+  						negPeakelement = bufferelement[a];
+  						negPeak = a;
+  					}
 				}
 				//vTaskDelay(10);
 				//Decode Buffer

a156ea73e7ed5b28b33cd5582fefe5189610ce1c
phschwenk <pschwenk@hotmail.de>
2022-12-06 17:43:19 +0100

    Update main.c
    -Kommentare Tast GetPeak ergänzt
    -Tas Getdifference implementiert

--- Receiver/QAMDecGen/main.c
+++ Receiver/QAMDecGen/main.c
@@ -40,8 +40,8 @@ void dataPointer(int mode, int adressNr, uint16_t *data);
 
 TaskHandle_t handler;
 
-uint16_t array1[22][32] = {NULL};	// zwischenlösung für 22 Waves und je 32Werte für weiterbearbeitung; sollte durch den Queue gefüllt werden
-uint16_t array2[22] = {NULL};		// den arrayplatz speichern an welcher Stelle der Peak ist für reveserse engineering welcher Bitwert
+uint16_t array1[28][32] = {NULL};	// zwischenlösung für 22 Waves und je 32Werte für weiterbearbeitung; sollte durch den Queue gefüllt werden
+uint16_t array2[28] = {NULL};		// den arrayplatz speichern an welcher Stelle der Peak ist für reveserse engineering welcher Bitwert
 
 void vApplicationIdleHook( void )
 {	
@@ -83,13 +83,13 @@ void GetPeak( void *pvParameters ) {												// Peaks aus dem Array mit allen
 	uint16_t actualPeak = 0;														// Zwischenspeicher des höchsten Werts
 	// How to get pointeradress[0] from different Task?
 	// Mutex damit diese Daten gesperrt sind während Bearbeitung
-	for (int a = 0; a >= 21; a++) {			// for 22 waves with data
-		for (int b = 0; b >= 31; b++) {		// for 32 samples per wave
-			if(array1[a][b] > actualPeak) {
-				actualPeak = array1[a][b];
-				array2[a] = b;
+	for (int a = 0; a <= 27; a++) {													// for 22 waves with data
+		for (int b = 0; b <= 31; b++) {												// for 32 samples per wave
+			if(array1[a][b] > actualPeak) {											// Finden vom Höchstwert der Welle das jeweils nur bei dem Höchstwert der steigenden Welle
+				actualPeak = array1[a][b];											// übergabe vom neuen Höchstwert
+				array2[a] = b;														// Position vom Höchstwert der welle wird gespeichert
 			}
-			actualPeak = 0;															// Für nächste Runde wider auf 0 damit wieder hochgearbeitet werden kann
+			actualPeak = 0;															// Für nächste Runde wieder auf 0 damit wieder hochgearbeitet werden kann
 		}
 		vTaskDelay(100/ portTICK_RATE_MS );
 	}
@@ -97,7 +97,94 @@ void GetPeak( void *pvParameters ) {												// Peaks aus dem Array mit allen
 	// vTaskSuspend;																// Damit keine Resourcen besetzt wenn nicht nötig
 }
 
-void GetDifference( void *pvParameters ) {
+void GetDifference( void *pvParameters ) {											// Task bestimmt die Zeit zwischen den höchstwerten der Wellen die im array2 gespeichert sind. 1 Messpunkt alle 31.25 uS.
+	uint32_t TimeTable[32] = [3125,6250,9375,12500,15625,18750,21875,25000,		// TimeTable wo die MessPunkte in 10^-8 Sekunden hinterlegt sind.
+							  28125,31250,34375,37500,40625,43750,46875,50000,
+							  53125,56250,59375,62500,65625,68750,71875,75000,
+							  78125,81250,84375,87500,90625,93750,96875,100000];		// Wir warscheindlich nicht benötigt 06.12.2022 PS
+	uint32_t HoechstwertPos1 = 0;													// Variable für aktuelle Position vom Höchstwert
+	uint32_t HoechstwertPos2 = 0;													// Variabel für nächste Position Höchstwert	
+	uint32_t DifferenzPos = 0;
+	uint8_t  WellenWert[28] = {NULL};												// Empfangen Daten in einem Array. Zugeortnet mit dem Wert 00 01 10 11 pro welle
+	
+	for(int i = 0; i <= 27; i ++){
+		HoechstwertPos1 = TimeTable[array2[i]];
+		HoechstwertPos2 = TimeTable[array2[i++]];
+		DifferenzPos = HoechstwertPos2-HoechstwertPos1
+		if(i<=3){
+			if(DifferenzPos = 100000){
+				WellenWert[i++] = 0;
+			}
+			if(DifferenzPos = 175000){
+				WellenWert[i++] = 2;
+			}
+			if(DifferenzPos = 150000){
+				WellenWert[i++] = 3;
+			}
+			if(DifferenzPos = 125000){
+				WellenWert[i++] = 1;
+			}
+		}
+		else{
+			if(WellenWert[0] == 0){
+				if(DifferenzPos = 100000){
+					WellenWert[i++] = 0;
+				}
+				if(DifferenzPos = 175000){
+					WellenWert[i++] = 2;
+				}
+				if(DifferenzPos = 150000){
+					WellenWert[i++] = 3;
+				}
+				if(DifferenzPos = 125000){
+					WellenWert[i++] = 1;
+				}
+			}
+			if(WellenWert[0] == 1){
+				if(DifferenzPos = 75000){
+					WellenWert[i++] = 0;
+				}
+				if(DifferenzPos = 150000){
+					WellenWert[i++] = 2;
+				}
+				if(DifferenzPos = 125000){
+					WellenWert[i++] = 3;
+				}
+				if(DifferenzPos = 100000){
+					WellenWert[i++] = 1;
+				}
+			}
+			if(WellenWert[0] == 2){
+				if(DifferenzPos = 25000){
+					WellenWert[i++] = 0;
+				}
+				if(DifferenzPos = 100000){
+					WellenWert[i++] = 2;
+				}
+				if(DifferenzPos = 75000){
+					WellenWert[i++] = 3;
+				}
+				if(DifferenzPos = 50000){
+					WellenWert[i++] = 1;
+				}
+			}
+			if(WellenWert[0] == 3){
+				if(DifferenzPos = 50000){
+					WellenWert[i++] = 0;
+				}
+				if(DifferenzPos = 125000){
+					WellenWert[i++] = 2;
+				}
+				if(DifferenzPos = 125000){
+					WellenWert[i++] = 3;
+				}
+				if(DifferenzPos = 100000){
+					WellenWert[i++] = 1;
+				}
+			}
+		}
+	}
+	
 	
 	vTaskDelay( 100 / portTICK_RATE_MS );
 	//vTaskSuspend;																	// Damit keine Resourcen besetzt wenn nicht nötig

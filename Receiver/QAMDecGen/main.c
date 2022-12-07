18117b1e4e3616b1429cb7f03e6cb7db0a206d0a
phschwenk <pschwenk@hotmail.de>
2022-12-07 12:17:21 +0100

    Debug
    Debug Code in Task GetPeak und Getdeference

--- Receiver/QAMDecGen/main.c
+++ Receiver/QAMDecGen/main.c
@@ -41,7 +41,7 @@ uint16_t *dataPointer(int mode, uint16_t data[15][32]);
 
 TaskHandle_t handler;
 
-uint16_t array1[28][32] = {NULL};	// zwischenl�sung f�r 22 Waves und je 32Werte f�r weiterbearbeitung; sollte durch den Queue gef�llt werden
+uint16_t array1[28][32] = {NULL};	// zwischenl�sung f�r 28 Waves und je 32Werte f�r weiterbearbeitung; sollte durch den Queue gef�llt werden
 uint16_t array2[28] = {NULL};		// den arrayplatz speichern an welcher Stelle der Peak ist f�r reveserse engineering welcher Bitwert
 
 void vApplicationIdleHook( void )
@@ -99,10 +99,10 @@ void GetPeak( void *pvParameters ) {												// Peaks aus dem Array mit allen
 }
 
 void GetDifference( void *pvParameters ) {											// Task bestimmt die Zeit zwischen den h�chstwerten der Wellen die im array2 gespeichert sind. 1 Messpunkt alle 31.25 uS.
-	uint32_t TimeTable[32] = [3125,6250,9375,12500,15625,18750,21875,25000,		// TimeTable wo die MessPunkte in 10^-8 Sekunden hinterlegt sind.
+	uint32_t TimeTable[32] = {3125,6250,9375,12500,15625,18750,21875,25000,			// TimeTable wo die MessPunkte in 10^-8 Sekunden hinterlegt sind.
 							  28125,31250,34375,37500,40625,43750,46875,50000,
 							  53125,56250,59375,62500,65625,68750,71875,75000,
-							  78125,81250,84375,87500,90625,93750,96875,100000];		// Wir warscheindlich nicht ben�tigt 06.12.2022 PS
+							  78125,81250,84375,87500,90625,93750,96875,100000};	// Wir warscheindlich nicht ben�tigt 06.12.2022 PS
 	uint32_t HoechstwertPos1 = 0;													// Variable f�r aktuelle Position vom H�chstwert
 	uint32_t HoechstwertPos2 = 0;													// Variabel f�r n�chste Position H�chstwert	
 	uint32_t DifferenzPos = 0;
@@ -111,75 +111,75 @@ void GetDifference( void *pvParameters ) {											// Task bestimmt die Zeit z
 	for(int i = 0; i <= 27; i ++){
 		HoechstwertPos1 = TimeTable[array2[i]];
 		HoechstwertPos2 = TimeTable[array2[i++]];
-		DifferenzPos = HoechstwertPos2-HoechstwertPos1
+		DifferenzPos = HoechstwertPos2-HoechstwertPos1;
 		if(i<=3){
-			if(DifferenzPos = 100000){
+			if(DifferenzPos == 100000){
 				WellenWert[i++] = 0;
 			}
-			if(DifferenzPos = 175000){
+			if(DifferenzPos == 175000){
 				WellenWert[i++] = 2;
 			}
-			if(DifferenzPos = 150000){
+			if(DifferenzPos == 150000){
 				WellenWert[i++] = 3;
 			}
-			if(DifferenzPos = 125000){
+			if(DifferenzPos == 125000){
 				WellenWert[i++] = 1;
 			}
 		}
 		else{
 			if(WellenWert[0] == 0){
-				if(DifferenzPos = 100000){
+				if(DifferenzPos == 100000){
 					WellenWert[i++] = 0;
 				}
-				if(DifferenzPos = 175000){
+				if(DifferenzPos == 175000){
 					WellenWert[i++] = 2;
 				}
-				if(DifferenzPos = 150000){
+				if(DifferenzPos == 150000){
 					WellenWert[i++] = 3;
 				}
-				if(DifferenzPos = 125000){
+				if(DifferenzPos == 125000){
 					WellenWert[i++] = 1;
 				}
 			}
 			if(WellenWert[0] == 1){
-				if(DifferenzPos = 75000){
+				if(DifferenzPos == 75000){
 					WellenWert[i++] = 0;
 				}
-				if(DifferenzPos = 150000){
+				if(DifferenzPos == 150000){
 					WellenWert[i++] = 2;
 				}
-				if(DifferenzPos = 125000){
+				if(DifferenzPos == 125000){
 					WellenWert[i++] = 3;
 				}
-				if(DifferenzPos = 100000){
+				if(DifferenzPos == 100000){
 					WellenWert[i++] = 1;
 				}
 			}
 			if(WellenWert[0] == 2){
-				if(DifferenzPos = 25000){
+				if(DifferenzPos == 25000){
 					WellenWert[i++] = 0;
 				}
-				if(DifferenzPos = 100000){
+				if(DifferenzPos == 100000){
 					WellenWert[i++] = 2;
 				}
-				if(DifferenzPos = 75000){
+				if(DifferenzPos == 75000){
 					WellenWert[i++] = 3;
 				}
-				if(DifferenzPos = 50000){
+				if(DifferenzPos == 50000){
 					WellenWert[i++] = 1;
 				}
 			}
 			if(WellenWert[0] == 3){
-				if(DifferenzPos = 50000){
+				if(DifferenzPos == 50000){
 					WellenWert[i++] = 0;
 				}
-				if(DifferenzPos = 125000){
+				if(DifferenzPos == 125000){
 					WellenWert[i++] = 2;
 				}
-				if(DifferenzPos = 125000){
+				if(DifferenzPos == 125000){
 					WellenWert[i++] = 3;
 				}
-				if(DifferenzPos = 100000){
+				if(DifferenzPos == 100000){
 					WellenWert[i++] = 1;
 				}
 			}
